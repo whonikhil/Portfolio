21a1c676fdb1ae895c3ec2c652129db6936d8046
Nikhil Kanojia <visualsbynikhil19@gmail.com>
2025-12-01 18:18:49 +0530

    Resolve all merge conflicts

--- client/src/globalTypes.ts
+++ client/src/globalTypes.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export interface Project {
   id: string;
   title: string;
@@ -12,19 +11,4 @@ export interface Project {
   repoUrl?: string;
   liveUrl?: string;
   featured: boolean;
-=======
-export interface Project {
-  id: string;
-  title: string;
-  category: 'Development' | 'Photography' | 'Videography';
-  shortDescription: string;
-  fullDescription?: string;
-  stack: string[];
-  coverImage: string;
-  images?: string[];
-  videoUrl?: string;
-  repoUrl?: string;
-  liveUrl?: string;
-  featured: boolean;
->>>>>>> 43f06c49a4eaa6df75cec31c851edc837136c9dc
 }
