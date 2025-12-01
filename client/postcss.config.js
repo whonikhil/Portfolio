30001d400ccf86089ce4b4cc62f0c43cf445dabb
Nikhil Kanojia <visualsbynikhil19@gmail.com>
2025-12-01 18:42:00 +0530

    Fix postcss config conflict

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
->>>>>>> 43f06c49a4eaa6df75cec31c851edc837136c9dc
 }
