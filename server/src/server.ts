2f9f562d56fe496ab8d4e45a615d84b3a9c8fb1f
Nikhil Kanojia <visualsbynikhil19@gmail.com>
2025-12-09 17:24:56 +0530

    Rename data file to portfolioData to fix 404 error

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
-import { projects } from './data/projectList';
+import { projects } from './data/portfolioData';
 
 // Load the .env file
 dotenv.config();
